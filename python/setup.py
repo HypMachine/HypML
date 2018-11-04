9774ae992bb4d0a2a67944c5d01ec5244c04453f
Blair Hudson <blairhudson@me.com>
2018-11-04 15:52:16 +1100

    setup script for installation

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'hypml',
+  packages = ['hypml'],
+  version = '0.0.1',
+  description = 'A utility for loading valid Hypothesis Markup Language (.hypml) documents.',
+  author = 'Blair Hudson',
+  author_email = 'blairhudson@me.com',
+  url = 'https://github.com/HypMachine/HypML', 
+  download_url = '',
+  keywords = ['data', 'machine', 'learning', 'science', 'algorithm', 'hypml'],
+  classifiers = [],
+)
